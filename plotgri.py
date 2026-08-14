9854c8b7aabd888af7bf8e36d108c5805f121cb0
Beining Zhu <soedat@umich.edu>
2026-08-13 22:46:16 -0500

    fix

--- plotgri.py
+++ plotgri.py
@@ -10,16 +10,16 @@ def readgri(fname):
     V = np.array([[float(s) for s in f.readline().split()] for n in range(Nn)])
     # read boundaries
     NB = int(f.readline())
-    B = [];
+    B = []
     Bname = []
     for i in range(NB):
-        s = f.readline().split();
+        s = f.readline().split(); 
         Nb = int(s[0]);
         Bname.append(s[2])
         Bi = np.array([[int(s) - 1 for s in f.readline().split()] for n in range(Nb)])
         B.append(Bi)
     # read elements
-    Ne0 = 0;
+    Ne0 = 0
     E = []
     while (Ne0 < Ne):
         s = f.readline().split();
